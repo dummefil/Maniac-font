185295de5a179591c9a37d60972ed74c3d834727
Billy Everyteen <dimatonkih@gmail.com>
2017-02-16 00:24:10 +0300

    adding settings

--- javascript.js
+++ javascript.js
@@ -1,11 +1,15 @@
-var text = document.getElementById("text");
-var generate = document.getElementById("generate");
-var div = document.getElementById("content");
-var color = {'red': '', 'green': '', 'blue': ''};
+var text = getElById("text");
+var generate = getElById("generate");
+var div = getElById("content");
+var input = getElsByTag("input");
+var fontSizeMin;
+var fontSizeMax;
+var radioColor;
+var colors = {'red': '', 'green': '', 'blue': ''};
+
 
 init();
-randomColorRGB();
-randomSize(20, 40);
+
 
 function attachBody(tag){
   return document.body.appendChild(tag);
@@ -16,66 +20,109 @@ function createEl(tag){
 function createText(text){
   return document.createTextNode(text);
 }
-
-generate.onclick = function(){
-  var array = document.getElementsByTagName("P");
-  if(array) remove("P");
-  init();
-  randomColorRGB();
-  randomSize(20, 40);
+function getElsByTag(tag){
+  return document.getElementsByTagName(tag); //return array
+}
+function getElById(id){
+  return document.getElementById(id);
 }
 
+generate.onclick = function(){
+  remove("P");
 
-function init(){
+  fontSizeMin = parseInt(getElById("fontSizeMin").value);
+  fontSizeMax = parseInt(getElById("fontSizeMax").value);
+
+  radioColor = input[0].checked ? input[0].value : input[1].value;
+  
+
+  init(fontSizeMin, fontSizeMax, radioColor);
+};
+
+
+function init(fMin, fMax, color){
+
+  if(!color) color = "RGB";
+
   var txt = text.value;
-  console.log(txt); 
+
   for (var i = 0; i < txt.length; i++){
     var t = createText(txt[i]);
     var p = createEl("P");
     p.appendChild(t);
     div.appendChild(p);
   }
+
+  if(color == "RGB")
+
+    randomColor("RGB");
+
+  else
+
+    randomColor("GREY");
+
+  randomSize(fMin, fMax);
 }
 
 
-function randomColorRGB(){
-  var array = document.getElementsByTagName("P");
+function randomColor(par){
+
+  var array = getElsByTag("P");
+
   for (var i = 0; i < array.length; i++){
-    color.red = random();
-    color.green = random();
-    color.blue = random();
-    array[i].style.color = "#" + color.red + color.green + color.blue;
+
+    if(par == "RGB"){
+      colors.red = random();
+      colors.green = random();
+      colors.blue = random();
+    }
+    else {
+      colors.red = colors.green = colors.blue = random();
+    }
+
+    array[i].style.color = "#" + colors.red + colors.green + colors.blue;
   }
 }
 
-function randomColorGREY(){
-  var array = document.getElementsByTagName("P");
-  for (var i = 0; i < array.length; i++){
-    color.red = color.green = color.blue = random();
-    array[i].style.color = "#" + color.red + color.green + color.blue;
-  }
-}
+
 
 function randomSize(min, max){
-  var array = document.getElementsByTagName("P");
+
+  if(!min) min = 20;
+  if(!max) max = 40;
+
+  var array = getElsByTag("P");
+
   for (var i = 0; i < array.length; i++){
-    var random = Math.floor(Math.random() * (max - min) + min);
-    array[i].style.fontSize = random + "px";
+    var r = Math.floor(Math.random() * (max - min) + min);
+    array[i].style.fontSize = r + "px";
   }
 }
 
 function random(){
+
     var r = Math.floor(Math.random() * 256);
+
     return r.toString(16);
+
 }
 
 //FIX ME PLS
+//OR KILL ME
 function remove(tag){
-  var array = document.getElementsByTagName(tag);
-  for(var j = 0; j < 10; j++){
-    for(var i = 0; i < array.length; i++){
-    array[i].parentNode.removeChild(array[i]);
+
+  var array = getElsByTag(tag);
+
+  if (array){
+    for(var j = 0; j < 10; j++){
+      for(var i = 0; i < array.length; i++){
+      array[i].parentNode.removeChild(array[i]);
+      }
     }
   } 
+
+  else return false;
+
   return false;
+
 }
