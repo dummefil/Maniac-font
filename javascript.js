54690ce847116325c523cacd321923e300835173
Dima Tonkih <dimatonkih@gmail.com>
2018-10-27 12:39:16 +0300

    roar feature, license update, some styles

--- javascript.js
+++ javascript.js
@@ -1,128 +1,108 @@
-var text = getElById("text");
-var generate = getElById("generate");
-var div = getElById("content");
-var input = getElsByTag("input");
-var fontSizeMin;
-var fontSizeMax;
-var radioColor;
-var colors = {'red': '', 'green': '', 'blue': ''};
+const text = $('#text');
+const generate = $('#generate');
+const content = $('#content');
+const colors = {'red': '', 'green': '', 'blue': ''};
+const roar = $('#roar');
 
+let cache = [];
 
-init();
+function $(tag){
+  if (document.querySelectorAll(tag).length === 1) {
+    return document.querySelector(tag);
+  } else {
+    return document.querySelectorAll(tag);
+  }
+}
 
-
-function attachBody(tag){
-  return document.body.appendChild(tag);
-}
-function createEl(tag){
-  return document.createElement(tag);
-}
-function createText(text){
-  return document.createTextNode(text);
-}
-function getElsByTag(tag){
-  return document.getElementsByTagName(tag); //return array
-}
-function getElById(id){
-  return document.getElementById(id);
-}
 
 generate.onclick = function(){
-  remove("P");
+  clear();
 
-  fontSizeMin = parseInt(getElById("fontSizeMin").value);
-  fontSizeMax = parseInt(getElById("fontSizeMax").value);
-
-  radioColor = input[0].checked ? input[0].value : input[1].value;
-  
+  const radioInput = $("[name=color]");
+  const fontSizeMinNode = $("#fontSizeMin");
+  const fontSizeMaxNode = $("#fontSizeMax");
+  const fontSizeMin = parseInt(fontSizeMinNode.value || fontSizeMinNode.placeholder);
+  const fontSizeMax = parseInt(fontSizeMaxNode.value || fontSizeMaxNode.placeholder);
+  const radioColor = radioInput[0].checked ? radioInput[0].value : radioInput[1].value;
 
   init(fontSizeMin, fontSizeMax, radioColor);
 };
 
 
-function init(fMin, fMax, color){
+function init(fMin, fMax, color = "RGB"){
 
-  if(!color) color = "RGB";
+  if (roar.checked) {
+    forEachInTextarea((unit) => {
+      const p = Math.random() > 0.5 ? 'р' : 'Р';
+      const textNode = document.createTextNode(p);
+      const pNode = document.createElement('p');
 
-  var txt = text.value;
+      pNode.appendChild(textNode);
+      cache.push(pNode);
+      content.appendChild(pNode);
+    })
+  } else {
+    forEachInTextarea((unit) => {
+      const textNode = document.createTextNode(unit);
+      const pNode = document.createElement('p');
 
-  for (var i = 0; i < txt.length; i++){
-    var t = createText(txt[i]);
-    var p = createEl("P");
-    p.appendChild(t);
-    div.appendChild(p);
+      pNode.appendChild(textNode);
+      cache.push(pNode);
+      content.appendChild(pNode);
+    })
   }
-
-  if(color == "RGB")
-
+  if(color === "RGB") {
     randomColor("RGB");
-
-  else
-
+  } else {
     randomColor("GREY");
-
+  }
   randomSize(fMin, fMax);
 }
 
 
-function randomColor(par){
-
-  var array = getElsByTag("P");
-
-  for (var i = 0; i < array.length; i++){
-
-    if(par == "RGB"){
+function randomColor(pair){
+  forEachInCache((unit) => {
+    if (pair === "RGB"){
       colors.red = random();
       colors.green = random();
       colors.blue = random();
-    }
-    else {
+    } else {
       colors.red = colors.green = colors.blue = random();
     }
+    unit.style.color = `#${colors.red}${colors.green}${colors.blue}`;
+  })
+}
 
-    array[i].style.color = "#" + colors.red + colors.green + colors.blue;
+function randomSize(min = 20, max = 40){
+  forEachInCache((unit) => {
+    let r = Math.floor(Math.random() * (max - min) + min);
+    unit.style.fontSize = `${r}px`;
+  })
+}
+
+function forEachInCache (cb) {
+  for (let i = 0; i < cache.length; i++) {
+    const unit = cache[i];
+    cb(unit, i, cache);
   }
 }
 
-
-
-function randomSize(min, max){
-
-  if(!min) min = 20;
-  if(!max) max = 40;
-
-  var array = getElsByTag("P");
-
-  for (var i = 0; i < array.length; i++){
-    var r = Math.floor(Math.random() * (max - min) + min);
-    array[i].style.fontSize = r + "px";
+function forEachInTextarea (cb) {
+  let txt = text.value;
+  for (let i = 0; i < txt.length; i++) {
+    const unit = txt[i];
+    cb(unit, i, txt)
   }
 }
+
 
 function random(){
-
-    var r = Math.floor(Math.random() * 256);
-
+    const r = Math.floor(Math.random() * 256);
     return r.toString(16);
-
 }
 
-//FIX ME PLS
-//OR KILL ME
-function remove(tag){
-
-  var array = getElsByTag(tag);
-
-  if (array){
-    for(var j = 0; j < 10; j++){
-      for(var i = 0; i < array.length; i++){
-      array[i].parentNode.removeChild(array[i]);
-      }
-    }
-  } 
-
-  else return false;
-
-  return false;
-
+function clear(){
+  const content = document.querySelector('#content');
+  content.textContent = '';
+  cache = [];
 }
